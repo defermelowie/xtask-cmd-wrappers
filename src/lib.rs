b4539a0616234bbbb7a438f9cc597de7d3bc051c
Lowie Deferme <lowie.deferme@kuleuven.be>
2025-05-05 11:36:19 +0200

    feat: add tests for Command impl of std's Command

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ use std::{ffi::OsStr, iter::Iterator};
 
 pub use xtask_cmdwrap_macro::cmd as command;
 
+// TODO: guard behind feature flags
+#[cfg(feature = "make")]
+pub mod make;
+
 pub trait Command: Into<std::process::Command> {
     /// Add an argument
     fn arg<S: AsRef<OsStr>>(&mut self, arg: S) -> &mut Self;
@@ -46,6 +50,52 @@ impl Command for std::process::Command {
     }
 }
 
-// TODO: guard behind feature flags
-#[cfg(feature = "make")]
-pub mod make;
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::process::Command as StdCommand;
+
+    #[test]
+    fn test_arg() {
+        let mut cmd = StdCommand::new("echo");
+        cmd.arg("hello");
+        assert_eq!(cmd.get_program().to_string_lossy(), "echo");
+        assert_eq!(cmd.get_args().next().unwrap().to_string_lossy(), "hello");
+    }
+
+    #[test]
+    fn test_args() {
+        let mut cmd = StdCommand::new("echo");
+        let args = vec!["hello", "world"];
+        cmd.args(args.iter());
+        assert_eq!(cmd.get_program().to_string_lossy(), "echo");
+        assert_eq!(cmd.get_args().next().unwrap().to_string_lossy(), "hello");
+        assert_eq!(cmd.get_args().nth(1).unwrap().to_string_lossy(), "world");
+    }
+
+    #[test]
+    fn test_arg_if_true() {
+        let mut cmd = StdCommand::new("echo");
+        cmd.arg_if("hello", true);
+        assert_eq!(cmd.get_program().to_string_lossy(), "echo");
+        assert_eq!(cmd.get_args().next().unwrap().to_string_lossy(), "hello");
+    }
+
+    #[test]
+    fn test_arg_if_false() {
+        let mut cmd = StdCommand::new("echo");
+        cmd.arg_if("hello", false);
+        assert_eq!(cmd.get_program().to_string_lossy(), "echo");
+        assert_eq!(cmd.get_args().next(), None);
+    }
+
+    #[test]
+    fn test_args_if() {
+        let mut cmd = StdCommand::new("echo");
+        let args = vec!["hello", "world"];
+        cmd.args_if(args.iter(), |arg| arg.to_string().starts_with("h"));
+        assert_eq!(cmd.get_program().to_string_lossy(), "echo");
+        assert_eq!(cmd.get_args().next().unwrap().to_string_lossy(), "hello");
+        assert_eq!(cmd.get_args().nth(1), None);
+    }
+}
